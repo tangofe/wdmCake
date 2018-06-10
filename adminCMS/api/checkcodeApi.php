21e9a78c2e2b3106e0a3891dedcd0452bc09d81f
tangofe <540548194@qq.com>
2018-06-10 12:59:16 +0800

    git api

--- adminCMS/api/checkcodeApi.php
+++
@@ -1,8 +0,0 @@
-<?php  
-session_start();
-$res="check_no";
-if(strtoupper($_GET['code'])==strtoupper($_SESSION['code'])){
-	exit;
-}
-echo $res;
-?>
--- adminCMS/api/codeApi.php
+++
@@ -1,5 +0,0 @@
-<?php  
-include("../controller/CodeImg.class.php");
-new CodeImg(4);
-
-?>
